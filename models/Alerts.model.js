433aa4767eed2abedece3b8d6b61703a9c9b09bc
Roman Kuptsov <romankuptsov@icloud.com>
2023-07-24 11:05:17 +0200

    models created

---
+++ models/Alerts.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const alertsSchema = new Schema(
+  {
+    createdBy: [{ type: Scheme.Types.ObjectId, ref: "User" }],
+    dangerLevel: { type: String, required: true },
+    status: { type: String, required: true },
+    message: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Alerts = mongoose.model("Alerts", alertsSchema);
+
+module.exports = { Alerts };
