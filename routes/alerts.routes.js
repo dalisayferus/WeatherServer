12129ac3de54c0739d736ac99c7e9e7129e35eec
Roman Kuptsov <romankuptsov@icloud.com>
2023-07-26 16:40:45 +0200

    updating status and deleting alert implemented

--- routes/alerts.routes.js
+++ routes/alerts.routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const Alerts = require("../models/Alerts.model");
 const alertsRouter = express.Router();
 
+// Getting alerts from the database
 alertsRouter.get("/alerts", async (req, res) => {
   try {
-    const alerts = await Alerts.find().populate("createdBy").sort("createdBy"); // Fetch all alerts and populate the createdBy field
+    const alerts = await Alerts.find().populate("createdBy").sort("createdBy");
     res.json(alerts);
   } catch (error) {
     console.error("Error fetching alerts:", error);
@@ -12,6 +13,7 @@ alertsRouter.get("/alerts", async (req, res) => {
   }
 });
 
+// Creating new alerts to the database
 alertsRouter.post("/alerts", async (req, res) => {
   try {
     const { body } = req;
@@ -20,13 +22,38 @@ alertsRouter.post("/alerts", async (req, res) => {
       message: body.message,
       dangerLevel: body.dangerLevel,
       createdBy: body.createdBy,
-      status: "Pending",
+      status: "pending",
     });
-
     console.log(alert);
-    res.json({ message: "alert created", alert });
+    res.json({ message: "Alert created", alert });
   } catch (error) {
     console.error("Error fetching alerts:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Updating the status of alerts
+alertsRouter.put("/alerts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const alert = await Alerts.findByIdAndUpdate(id, { status }, { new: true });
+    console.log("Updated alert:", alert);
+    res.json(alert);
+  } catch (error) {
+    console.error("Error updating alert status:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+alertsRouter.delete("/alerts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Alerts.findByIdAndRemove(id);
+    console.log("Deleted alert:", id);
+    res.json({ message: "Alert deleted" });
+  } catch (error) {
+    console.error("Error deleting alert:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
